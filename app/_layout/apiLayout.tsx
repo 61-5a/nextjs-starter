ec0f1b825823581b32ff3de8943d0b8794ae9231
Rabih Aslam K <azaslam121@gmail.com>
2024-05-14 01:20:18 +0530

    PWA update

--- app/_layout/apiLayout.tsx
+++ app/_layout/apiLayout.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React, { useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { StoreContext } from "../_context/storeContext";
 
@@ -7,10 +8,47 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient();
 
+let deferredPrompt: any;
+
+import styles from "./apiLayout.module.css";
+
 export default function ApiLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   const Store = useContext(StoreContext);
   if (!Store) throw new Error("Store is not available in the context.");
   const setHistory: React.Dispatch<React.SetStateAction<string[]>> = Store?.setHistory;
+
+  const [installable, setInstallable] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI notify the user they can install the PWA
+      setInstallable(true);
+    });
+
+    window.addEventListener("appinstalled", () => {
+      // Log install to analytics
+      console.log("INSTALL: Success");
+    });
+  }, []);
+
+  const handleInstallClick = (e: any) => {
+    // Hide the app provided install promotion
+    setInstallable(false);
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult: { outcome: string }) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the install prompt");
+      } else {
+        console.log("User dismissed the install prompt");
+      }
+    });
+  };
 
   const pathname = usePathname();
   useEffect(() => {
@@ -28,6 +66,11 @@ export default function ApiLayout({ children }: Readonly<{ children: React.React
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      {installable && (
+        <button onClick={handleInstallClick} className={styles.install_button_footer}>
+          <Image src="/icons/download.svg" alt="download" width={40} height={40} title="Download this App" />
+        </button>
+      )}
       <ReactQueryDevtools initialIsOpen />
     </QueryClientProvider>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { StoreContextProvider } from "./_context/storeContext";
 import ApiLayout from "./_layout/apiLayout";
 
@@ -16,15 +16,80 @@ export const metadata: Metadata = {
   themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
   authors: [
     {
-      name: "imvinojanv",
-      url: "https://www.linkedin.com/in/imvinojanv/",
+      name: "aZ",
+      url: "https://github.com/61-5a/",
     },
   ],
-  viewport: "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
-  icons: [
-    { rel: "apple-touch-icon", url: "icons/icon.png" },
-    { rel: "icon", url: "icons/icon.png" },
-  ],
+  icons: {
+    icon: [
+      { url: "/icons/logo/icon.png" },
+      { url: "/icons/logo/icon-dark.png", media: "(prefers-color-scheme: dark)" },
+    ],
+    shortcut: ["/icons/shortcut-icon.png"],
+    apple: [
+      { url: "/icons/logo/apple-icon.png" },
+      { url: "/icons/logo/apple-icon-x152.png", sizes: "152x152", type: "image/png" },
+      { url: "/icons/logo/apple-icon-x180.png", sizes: "180x180", type: "image/png" },
+      { url: "/icons/logo/apple-icon-x167.png", sizes: "167x167", type: "image/png" },
+    ],
+    other: [
+      {
+        rel: "apple-touch-icon-precomposed",
+        url: "/icons/logo/apple-touch-icon-precomposed.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        url: "/icons/logo/icon-128x128.png",
+      },
+      {
+        rel: "icon",
+        url: "/icons/logo/icon-128x128.png",
+      },
+    ],
+  },
+  itunes: {
+    appId: "myAppStoreID",
+    appArgument: "myAppArgument",
+  },
+  appleWebApp: {
+    title: "Apple Web App",
+    statusBarStyle: "black-translucent",
+    startupImage: [
+      "/icons/logo/apple-touch-startup-image-768x1004.png",
+      {
+        url: "/icons/logo/apple-touch-startup-image-1536x2008.png",
+        media: "(device-width: 768px) and (device-height: 1024px)",
+      },
+      {
+        url: "/icons/logo/apple-touch-startup-image-2048x2048.png",
+        media: "(device-width: 2048px) and (device-height: 2732px)",
+      },
+    ],
+  },
+};
+
+//       <link rel="apple-touch-icon" href="touch-icon-iphone.png" />
+//       <link rel="apple-touch-icon" sizes="152x152" href="touch-icon-ipad.png" />
+//       <link rel="apple-touch-icon" sizes="180x180" href="touch-icon-iphone-retina.png" />
+//       <link rel="apple-touch-icon" sizes="167x167" href="touch-icon-ipad-retina.png" />
+
+//       <meta name="apple-mobile-web-app-capable" content="yes" />
+//       <link href="/apple_splash_2048.png" sizes="2048x2732" rel="apple-touch-startup-image" />
+//       <link href="/apple_splash_1668.png" sizes="1668x2224" rel="apple-touch-startup-image" />
+//       <link href="/apple_splash_1536.png" sizes="1536x2048" rel="apple-touch-startup-image" />
+//       <link href="/apple_splash_1125.png" sizes="1125x2436" rel="apple-touch-startup-image" />
+//       <link href="/apple_splash_1242.png" sizes="1242x2208" rel="apple-touch-startup-image" />
+//       <link href="/apple_splash_750.png" sizes="750x1334" rel="apple-touch-startup-image" />
+//       <link href="/apple_splash_640.png" sizes="640x1136" rel="apple-touch-startup-image" />
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  // Also supported by less commonly used
+  // interactiveWidget: 'resizes-visual',
 };
 
 export default function RootLayout({
